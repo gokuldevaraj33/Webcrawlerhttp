d40b5daed1af7de3e6e910fc50c4548b54c52c93
Gokul Devarajan <gokuldevaraj4@gmail.com>
2025-06-23 11:09:05 -0400

    getURLFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,43 @@
+const { url } = require('inspector');
+const {JSDOM} = require('jsdom');
+
+function getURLFromHTML(htmlbody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlbody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative url
+            try{
+
+            const urlobj = new URL(`${baseURL}${linkElement.href}`)
+            urls.push(urlobj.href)
+
+            }catch(err) {
+                console.log(`error with relative url: ${err.message} `)
+            }
+           
+        }
+        else {
+             // absolute url
+            try{
+
+            const urlobj = new URL(linkElement.href)
+            urls.push(urlobj.href)
+
+            }catch(err) {
+                console.log(`error with absolute url: ${err.message} `)
+            }
+           
+
+        }
+    }
+    return urls
+    
+}
+
+
 function normalizeUrl(urlString) {
     const urlobj = new URL(urlString)
     const hostpath = `${urlobj.hostname}${urlobj.pathname}`
@@ -8,6 +48,6 @@ function normalizeUrl(urlString) {
 }
 
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getURLFromHTML,
 }
- 
